d1afd5313caa8ab351fc2ee77c7fd28b78234a06
ht-yarll <h.scott.xp@gmail.com>
2025-01-03 20:32:26 -0300

    :construction: new module for data treat

--- src/main.py
+++ src/main.py
@@ -1,8 +1,55 @@
 import io
 import os
+import pathlib
 
-from modules.GCStorage import GStorage
-from modules.GBQuery import GBigQuery
+from modules.DataProcess import DataProcess
+from modules.GCStorage import GStorage, get_gclient
+from modules.GBQuery import GBigQuery, get_bqclient
 
 import pandas as pd
 
+working_dir = pathlib.Path.cwd()
+files_folder = working_dir.joinpath('data/raw')
+treated_files_folder = working_dir.joinpath('data/treated')
+processor = DataProcess(output_folder=treated_files_folder)
+
+
+#Gettig files, transforming them into Dataframe, saving
+file_path_list = []
+
+for file in files_folder.iterdir():
+    if file.is_file():
+        file_path_list.append(file)
+
+df_read_csv = (
+    pd.read_csv(files, encoding='utf8') 
+    for files in file_path_list
+)
+
+df = (
+    pd.concat(df_read_csv, ignore_index=True) 
+    if len(file_path_list) > 1 else file_path_list[0]
+)
+
+treated_df = processor.treat_data(df)
+processor.save_to_parquet(treated_df)
+
+
+# #up to gcloud
+storage_client = get_gclient()
+gcs = GStorage(storage_client)
+bucket_name = 'blackstone-churn'
+
+try:
+    if not bucket_name in gcs.list_buckets():
+        bucket_gcs = gcs.create_bucket('blackstone-churn', storage_class='STANDARD')
+    else:
+        bucket_gcs = gcs.get_bucket(bucket_name)
+
+    for file_path in treated_files_folder.glob('*.*'):
+        gcs.upload_file(bucket_gcs, file_path.name, str(file_path))
+    
+    print(f'upload of {file_path.name} was done!')
+
+except Exception as e:
+    print(e)
---
+++ src/modules/DataProcess.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import pandas as pd
+
+class DataProcess:
+    def __init__(self, output_folder: Path):
+        self.output_folder = output_folder
+
+    def treat_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        try:
+            print('Treating data...')
+            df.columns = (
+                df.columns
+                .str.strip()
+                .str.lower()
+                .str.replace(r"[^a-z0-9_]", "_", regex=True)
+
+            )
+            df = df.fillna({
+                col: "N/A" if df[col].dtype == "object" else 0
+                for col in df.columns
+            })
+        except Exception as e:
+            print(f'Error during treating data: {e}')
+
+        return df
+
+    def save_to_parquet(self, df: pd.DataFrame, file_name: str = 'table.parquet'):
+        try:
+            output_path = self.output_folder.joinpath(file_name)
+            print('saving df to parquet on folder')
+            df.to_parquet(
+                output_path,
+                compression = None
+            )
+            print(f'File saved on {output_path} with success')
+        
+        except Exception as e:
+            print(f'Error during save: {e}')
