2f6c655044ab68fae5e6bbe59a6329956809cd29
ht-yarll <h.scott.xp@gmail.com>
2025-01-03 17:42:03 -0300

    :construction:

--- src/up_parquet_GS.py
+++ src/up_parquet_GS.py
@@ -6,14 +6,16 @@ from modules.GCStorage import GStorage, get_gclient
 from google.cloud import bigquery
 from google.cloud import storage
 import pandas as pd
+import pyarrow
 
 
+#variables
 working_dir = pathlib.Path.cwd()
 files_folder = working_dir.joinpath('data/raw')
 treated_files_folder = working_dir.joinpath('data/treated')
 
 
-#Gettig files and transforming them into Dataframe
+#Gettig files, transforming them into Dataframe, saving
 file_path_list = []
 
 for file in files_folder.iterdir():
@@ -29,3 +31,38 @@ df = (
     pd.concat(df_read_csv, ignore_index=True) 
     if len(file_path_list) > 1 else file_path_list[0]
 )
+
+
+def treat_data(df):
+    try:
+        print('Treating data...')
+        df.columns = (
+            df.columns
+            .str.strip()
+            .str.lower()
+            .str.replace(r"[^a-z0-9_]", "_", regex=True)
+
+        )
+        df = df.fillna({
+            col: "N/A" if df[col].dtype == "object" else 0
+            for col in df.columns
+        })
+    except Exception as e:
+        print(e)
+
+    return df
+
+def save_to_parquet(df):
+    try:
+        print(f'saving df to parquet on {treated_files_folder}')
+        df.to_parquet(
+            treated_files_folder.joinpath('table.parquet'),
+            compression = None
+        )
+        return f'file saved on {treated_files_folder} with succes'
+    
+    except Exception as e:
+        print(e)
+
+treat_data(df)
+save_to_parquet(df)
