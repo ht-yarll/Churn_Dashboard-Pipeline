49a861310cc3e24cdc0be9e6c1e981982aef792b
ht-yarll <h.scott.xp@gmail.com>
2025-01-14 20:09:01 -0300

    :white_check_mark: test: merge method success

---
+++ src/test_for_df_merge.py
@@ -0,0 +1,49 @@
+import pathlib
+
+from modules.DataProcess import DataProcessor
+from modules.GCStorage import GCStorage, get_gclient
+from modules.GBQuery import GBigQuery, get_bqclient
+
+
+import pandas as pd
+
+working_dir = pathlib.Path.cwd()
+files_folder = working_dir.joinpath('data/raw')
+output_folder = working_dir.joinpath('data/treated')
+processor = DataProcessor(output_path=output_folder)
+
+
+# df = pd.read_csv(files_folder.joinpath('Location_Data.csv'))
+
+# df['city'] = df['city'].str.strip()
+
+# # Check for leading or trailing spaces in the 'city' column
+# print(df['city'].head())  # Print sample values
+# print(df['city'].apply(lambda x: f"'{x}'"))  # Highlight spaces in the values
+
+
+file_path_list = []
+
+for file in files_folder.iterdir():
+    if file.is_file():
+        file_path_list.append(file)
+
+df_read_csv = (
+    pd.read_csv(files, header=0, encoding='utf8') 
+    for files in file_path_list
+)
+
+df_read_csv = list(df_read_csv)
+result = df_read_csv[0]
+
+for df in df_read_csv[1:]:
+    result = pd.merge(result, df, on='customer_id' )
+
+
+# df = (
+#     pd.concat(df_read_csv, axis=1) 
+#     if len(file_path_list) > 1 
+#     else pd.read_csv(file_path_list[0], encoding='utf8')
+# )
+
+print(result)
